9f9a3dc55b90f80040861a7fc180965c447aa556
Gabriel Rozendo <79455923+rozendox@users.noreply.github.com>
2023-05-12 01:15:25 -0300

    Create const.cpp

---
+++ const.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+    const double PI = 3.14159;
+    const int LIGHT_SPEED = 299792458;
+    const int widht = 1920;
+    const int height = 1080;
+    
+    return 0;
+}
