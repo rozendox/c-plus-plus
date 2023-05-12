6952eb110f0611712dded07ad426479248736cfd
Gabriel Rozendo <79455923+rozendox@users.noreply.github.com>
2023-05-12 01:09:25 -0300

    Create tipos.cpp

---
+++ tipos.cpp
@@ -0,0 +1,37 @@
+//Rox
+#include <iostream>
+
+int main(){
+    
+    int age = 21;
+    int year = 2023;
+    int days = 7;
+
+
+    // double number including decimal
+    double price = 10.99;
+    double gpa = 2.5;
+    double temperature = 25.1;
+
+    // single caracter - can store one single caracter 
+    char grade = 'A';
+    char initial = 'B';
+    char currency = '$';
+
+    //boolean ( true of false)
+
+    bool student = false;
+    bool power = true;
+    bool forSale = false;
+
+    //string (objects that represents a sequence of text)
+
+    std::string name = "Rox";
+    std::string day = "Friday";
+    std::string food = "pizza";
+    std::string adress = "123 Fake St.";
+
+    std::cout << "Hello " << name << '\n';
+    std::cout << "Your are " << age << " years old" ; 
+    return 0;
+}
