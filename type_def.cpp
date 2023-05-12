c45bf72fb2a1e94f730f32df0876b066253ee5fe
Gabriel Rozendo <79455923+rozendox@users.noreply.github.com>
2023-05-12 01:28:16 -0300

    Update type_def.cpp

--- type_def.cpp
+++ type_def.cpp
@@ -1,23 +1,23 @@
-//Rox 
-
 #include <iostream>
 #include <vector>
 
 //typedef std::vector<std::pair<std::string, int>> pairlist_t;
 typedef std::string text_t;
-
+typedef int number_t;
 int main(){
     
     text_t firstName = "Rox";
-    
+    number_t age = 24;
     std::cout << firstName << '\n';
-    
+    std::cout << age << '\n';
     return 0;
 }
 
 /*
 [Running] cd "c:===============" && g++ helloworld.cpp -o helloworld && "c:============"helloworld
 Rox
+24
 
-[Done] exited with code=0 in 0.518 seconds
+[Done] exited with code=0 in 0.623 seconds
+
 */
