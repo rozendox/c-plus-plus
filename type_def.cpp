c360c7984c3292d1ea789d4ca38a4b954cd77b65
Gabriel Rozendo <79455923+rozendox@users.noreply.github.com>
2023-05-12 01:26:59 -0300

    Create type_def.cpp

---
+++ type_def.cpp
@@ -0,0 +1,23 @@
+//Rox 
+
+#include <iostream>
+#include <vector>
+
+//typedef std::vector<std::pair<std::string, int>> pairlist_t;
+typedef std::string text_t;
+
+int main(){
+    
+    text_t firstName = "Rox";
+    
+    std::cout << firstName << '\n';
+    
+    return 0;
+}
+
+/*
+[Running] cd "c:===============" && g++ helloworld.cpp -o helloworld && "c:============"helloworld
+Rox
+
+[Done] exited with code=0 in 0.518 seconds
+*/
